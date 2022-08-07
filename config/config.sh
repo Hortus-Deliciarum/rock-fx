6f16ae9e138d329539389bd015a106708599ab92
franeum <elettrochuck@gmail.com>
2022-08-07 17:04:27 +0200

    update

--- config/config.sh
+++ config/config.sh
@@ -2,9 +2,12 @@
 
 cp rotary_client.service /etc/systemd/system/.
 cp rotary_server.service /etc/systemd/system/.
+cp rock_jackd.service /etc/systemd/system/.
 systemctl start rotary_client
 systemctl start rotary_server
+systemctl start rock_jackd
 systemctl enable rotary_client
 systemctl enable rotary_server
+systemctl enable rock_jackd
 echo "running daemon-reload..."
 systemctl daemon-reload
