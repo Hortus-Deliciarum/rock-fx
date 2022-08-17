e6a6695f0061ac9e08d0f07ceac43a65d9600292
franeum desk <elettrochuck@gmail.com>
2022-08-17 16:49:29 +0200

    add menu list

--- config/config.sh
+++ config/config.sh
@@ -8,16 +8,58 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 
 DIR_SYSTEMD="/etc/systemd/system"
-DIR_HOMEROCK="/home/rock"
+DIR_HOMEROCK="/home/neum"
+DIR_PATCH="$DIR_HOMEROCK/Documenti/Hortus-Deliciarum/rock-fx/patches"
+PUREDATA=".puredata"
 COPY_SERVICES=( "rotary_client.service" "rotary_server.service" "rock_jackd.service" "rock_puredata.service" )
 COPY_FILES=( ".jackdrc" ".puredata" )
 SERVICES=( "rotary_client" "rotary_server" "rock_jackd" "rock_puredata")
+FILES=(`ls -C $DIR_PATCH/*.pd`)
+
+length=${#files[@]}
+counter=1
+
+set_patch() {
+    echo "#!/usr/bin/env bash" > $DIR_HOMEROCK/$PUREDATA
+    echo puredata -nogui -jack $1 > $DIR_HOMEROCK/$PUREDATA
+}
+
+# BANNER
 
 clear
 echo
 figlet -ck -f pagga "Lem in Rock"
 figlet -ck -f future configuration
 sleep 2
+
+# CHOOSE PATCH
+
+echo 
+for i in ${FILES[@]}; do 
+    #echo "$counter) $i" 
+    echo "($counter) $(basename $i)"
+    counter=$(( $counter + 1))
+done 
+
+control=1
+echo 
+
+while [[ control -eq 1 ]]; do 
+    printf "Choose a patch by index: "
+    read n
+    index=$(( $n - 1 ))
+    
+    if [[ $index -gt $(( $length - 1 )) || $index -lt 0 ]]
+        then 
+            echo "Index out of range, please retry..."
+        else
+            set_patch ${files[$index]}
+            control=0
+    fi 
+done
+
+# OPERATIONS
+
 echo
 echo "=== COPYING SERVICES AND FILES... ==="
 sleep 1
